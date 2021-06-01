c65a31cc65f1ef64297ba72b411623660c5173a3
benedict <closer9554@gmail.com>
2021-06-01 17:53:55 +0900

    6/1 객체지향

---
+++ src/com/javaex/Ex01/Goods.java
@@ -0,0 +1,10 @@
+package com.javaex.Ex01;
+
+public class Goods {
+
+	String name;
+	int price;
+
+	
+
+}
---
+++ src/com/javaex/ex00/Ex01.java
@@ -0,0 +1,20 @@
+package com.javaex.ex00;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		String name = "니콘";
+		int price = 40000;
+		System.out.println("카메라"+name+":"+price);
+		
+		String name2="그램";
+		int price2 = 10000000;
+		System.out.println("노트북"+name2+":"+price2);
+		*/
+		
+		
+	}
+
+}
